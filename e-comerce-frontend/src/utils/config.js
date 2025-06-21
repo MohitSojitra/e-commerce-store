0ce071a639476160ef557360af94bd64835ab0ef
Mohit Sojitra <mohitsojitra0@gmail.com>
2025-06-21 14:40:25 +0530

    Migration run

--- e-comerce-frontend/src/utils/config.js
+++ e-comerce-frontend/src/utils/config.js
@@ -1,9 +1,9 @@
 const devConfig = {
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
-};
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+}
 
 const prodConfig = {
-  baseURL: "Your production url",
-};
+  baseURL: 'Your production url',
+}
 
-export const config = devConfig;
+export const config = devConfig
