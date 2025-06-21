107691fcd59d15d34b6a8a8cf23b08ea0127a1af
Mohit Sojitra <47450328+MohitSojitra@users.noreply.github.com>
2025-06-21 15:17:01 +0530

    Url log added

--- e-comerce-backend/backend/config/db.js
+++ e-comerce-backend/backend/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
+    console.log({MONGO_URI:process.env.MONGO_URI})
     await mongoose.connect(
       process.env.MONGO_URI || 'mongodb://127.0.0.1/ecom',
       {
